27f98a6f5d9c0303b28e80321c3521e758107043
Pavel Koryakin <pkoryakin@devteam.powerhub.com>
2018-06-20 15:36:32 -0400

    removed extra files

--- src/shared/app-settings.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { AppSettings } from "./app.settings";
-import { Observable,of } from 'rxjs';
-@Injectable()
-export class AppSettingsService {
-  getSettings(): AppSettings {
-    return new AppSettings()
-  }
-}
--- src/shared/app.settings.ts
+++
@@ -1,4 +0,0 @@
-export class AppSettings {
-    readonly API_URL: string = "http://localhost:51117/api"
-    readonly PAGE_SIZE: number = 3;
-}
