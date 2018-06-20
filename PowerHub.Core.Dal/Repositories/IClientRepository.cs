30083c94306909627a0b43483eb2491da3930bca
Pavel Koryakin <pkoryakin@devteam.powerhub.com>
2018-06-20 15:35:39 -0400

    removed extra files

--- PowerHub.Core.Dal/Repositories/IClientRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using PowerHub.Core.Dal.ClientDbEntities.PowerHubClientEntities;
-using PowerHub.Core.Utils.EntityTypes;
-
-namespace PowerHub.Core.Dal.Repositories
-{
-    public interface IClientRepository<T> : IRepository<T, PH_TrainingContext>
-        where T : class, IEntity
-    { }
-}
